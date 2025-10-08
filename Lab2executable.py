75b04123da629381b696183b7c297d388b173759
acebird <acebird15@gmail.com>
2025-10-08 07:39:48 -0400

    Executable python code

---
+++ Lab2executable.py
@@ -0,0 +1,33 @@
+import fileinput
+from pathlib import Path
+import socket
+
+
+# Create Folder
+log_dir = Path("/var/logs")
+log_dir.mkdir(parents=True, exist_ok=True)
+
+# Create File
+log_file = log_dir / "file.log"
+log_file.touch(exist_ok=True)
+
+
+# Edit CSS file
+path = Path("/etc/dizquetv/.dizquetv/custom.css")
+
+# Read, replace, and write back
+text = path.read_text()
+text = text.replace("--guide-text : #F0F0f0:", "--guide-text : #b14194:")
+path.write_text(text)
+
+HOST = "0.0.0.0"   # listen on all interfaces
+PORT = 5000        # choose any test port you like
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+    
+    while True:
+        conn, addr = s.accept()
+        print(f"Connection from {addr}")
+        conn.close()
